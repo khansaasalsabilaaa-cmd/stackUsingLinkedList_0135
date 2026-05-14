6b154cd23f3d4345a4d0a8ca244ddcbcba7e5a78
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-14 10:55:29 +0700

    menambahkan case default

--- StackC++.cpp
+++ StackC++.cpp
@@ -118,7 +118,10 @@ int main()
         case 4:
             cout << "Exiting program." << endl;
             break;
-        
+        default:
+            cout << "invalid choice. Try again." << endl;
+            break;
         }
+        
     }
 }
