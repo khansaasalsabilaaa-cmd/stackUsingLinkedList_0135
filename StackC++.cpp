de0112779b3012f9a9469c0d459088bc95128443
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-14 10:54:19 +0700

    menambahkan case 4

--- StackC++.cpp
+++ StackC++.cpp
@@ -115,6 +115,9 @@ int main()
                 cout << "Stack is empty.No top value." << endl;
             }
             break;
+        case 4:
+            cout << "Exiting program." << endl;
+            break;
         
         }
     }
