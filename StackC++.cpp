4d93a2f6d79eb151a070e179ed31e0e32debba10
khansaasasabilaaa-eng <khansaasasabilaaa@gmail.com>
2026-05-14 10:51:08 +0700

    menambahkan case 2

--- StackC++.cpp
+++ StackC++.cpp
@@ -95,6 +95,16 @@ int main()
             cin >> value;
             stack.push(value);
             break;
+        case 2:
+            if (!stack.isEmpty())
+            {
+                stack.pop();
+            }
+            else
+            {
+                cout << "Stack is empty.No top value." << endl;
+            }
+            break;
         
         }
     }
